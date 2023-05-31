a04146efbfdb76878ca02a985a28d1ebed804d60
zzp1012 <zzp1012@sjtu.edu.cn>
2023-05-31 14:40:31 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,11 @@
 # !/bin/bash
 
 python main.py --save_root ../outs/predictions/kmeans \
-               --seed 0 \
                --model kmeans \
                --n_clusters 3991 \
                -v
 
 python main.py --save_root ../outs/predictions/HAC \
-               --seed 0 \
                --model HAC \
                --n_clusters 3991 \
                -v
